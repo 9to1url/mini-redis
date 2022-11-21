93c158998b3904d2c6de5d5eb7ac072eed133f09
gitusername <gitemail@example.com>
2022-11-21 11:29:42 -0500

    increase max connection to 25000

--- src/server.rs
+++ src/server.rs
@@ -110,7 +110,7 @@ struct Handler {
 /// production (you'd think that all the disclaimers would make it obvious that
 /// this is not a serious project... but I thought that about mini-http as
 /// well).
-const MAX_CONNECTIONS: usize = 250;
+const MAX_CONNECTIONS: usize = 25000; // orig 250
 
 /// Run the mini-redis server.
 ///
